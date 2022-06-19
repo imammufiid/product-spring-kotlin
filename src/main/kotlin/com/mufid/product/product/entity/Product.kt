59f205d85146b7d8f911177e73c70188cea5e929
imufiid <imufiid98@gmail.com>
2022-06-19 15:46:20 +0700

    Setup entity

---
+++ src/main/kotlin/com/mufid/product/product/entity/Product.kt
@@ -0,0 +1,13 @@
+package com.mufid.product.product.entity
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.litote.kmongo.Id
+import org.litote.kmongo.newId
+
+data class Product(
+    @BsonId
+    var _id: Id<Product> = newId(),
+    var name: String? = "",
+    var quantity: String? = "",
+    var price: Int? = 0
+)
